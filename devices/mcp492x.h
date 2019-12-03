fe600046c45f2479c4978a263dc45e772e760875
Jan-Henrik Bruhn <hi@jhbruhn.de>
2019-12-04 00:03:38 +0100

    Support all 12 bits for MCP492x

--- devices/mcp492x.h
+++ devices/mcp492x.h
@@ -49,24 +49,23 @@ class Dac {
     Interface::Init();
   }
 
-  static inline void Write(uint8_t value) {
+  static inline void Write(uint16_t value) {
     Write(value, 0);
   }
 
-  static inline void Write(uint8_t value, uint8_t channel) {
-    value = U8Swap4(value);
+  static inline void Write(uint16_t value, uint8_t channel) {
     uint8_t command;
-    command = (value & 0x0f) | 0x10;
+    command = ( 0x10 | (value >> 8));
     if (channel) {
       command |= 0x80;
     }
-    if (voltage_reference == BUFFERED) {
+    if (voltage_reference == BUFFERED_REFERENCE) {
       command |= 0x40;
     }
-    if (gain == 1) {
+    if (gain) {
       command |= 0x20;
     }
-    Interface::WriteWord(command, value & 0xf0);
+    Interface::WriteWord(command, value & 0xff);
   }
 };
 
